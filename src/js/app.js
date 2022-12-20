9b6cd62fcc4314fabec02103ebaa0cead2f14d73
shahrizoda <shahrizodadeveloping@gmail.com>
2022-12-20 22:06:54 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -32,3 +32,21 @@ menu.addEventListener('click', (e)=>{
     menu.classList.toggle('active')
     content.classList.toggle('active')
 })
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
